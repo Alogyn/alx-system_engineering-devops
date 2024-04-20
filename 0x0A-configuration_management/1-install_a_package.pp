70b3f7d0796dacef318bf0510ec4a7c6e751399d
Mohamed Derfoufi <138708102+Alogyn@users.noreply.github.com>
2024-04-20 02:04:18 +0100

    Using Puppet, install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # Install an especific version of flask (2.1.0)
-exec { 'install python packages':
-     command   => 'pip3 install flask flask_restful apiai',
-     path => ['/usr/bin/'],
-     unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
-  }
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
