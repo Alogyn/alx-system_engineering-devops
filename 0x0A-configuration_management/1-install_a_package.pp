7efa120769b6a4f8c60ead076dd9267e5976d57b
Mohamed Derfoufi <138708102+Alogyn@users.noreply.github.com>
2024-04-20 01:53:51 +0100

    Using Puppet, install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Install an especific version of flask (2.1.0)
 exec { 'install-flask':
-  command => 'pip3 install Flask==2.1.0',
-  unless  => 'pip3 freeze | grep Flask==2.1.0',
+  command => 'pip3 install flask==2.1.0',
+  unless  => 'pip3 freeze | grep flask==2.1.0',
   path    => ['/bin', '/usr/bin', '/usr/local/bin'],
 }
