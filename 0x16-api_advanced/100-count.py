e374850f41999b0fce71601fbfa1ef6e449def11
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-06-04 13:19:05 -0700

    Recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords (case-insensitive, delimited by spaces

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,30 +5,34 @@ Contains the count_words function
 import requests
 
 
-def count_words(subreddit, word_list, after=None, counts={}):
+def count_words(subreddit, word_list, after=None, counts=None):
     """
-    Queries the Reddit API and counts the occurrences of keywords in the titles of all hot posts.
-    Prints the counts sorted by count (descending) and then alphabetically (ascending).
+    Queries the Reddit API and counts the occurrences of keywords in the
+    titles of all hot posts. Prints the counts sorted by count (descending)
+    and then alphabetically (ascending).
 
     :param subreddit: the name of the subreddit
     :param word_list: list of keywords to count
     :param after: the next page token for pagination
     :param counts: dictionary to accumulate counts of keywords
     """
-    if after is None:
+    if counts is None:
         # Initialize counts dictionary with lowercase keywords
         counts = {word.lower(): 0 for word in word_list}
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)"
+        "User-Agent": "0x16-api_advanced:project: \
+        v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
     params = {
         "after": after,
         "limit": 100
     }
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(
+        url, headers=headers, params=params, allow_redirects=False
+    )
     if response.status_code == 404:
         return
 
@@ -48,9 +52,11 @@ def count_words(subreddit, word_list, after=None, counts={}):
     if after is not None:
         count_words(subreddit, word_list, after, counts)
     else:
-        # Sorting the counts dictionary by value (descending) and then by key (ascending)
-        sorted_counts = sorted(counts.items(), key=lambda item: (-item[1], item[0]))
+        # Sorting the counts dictionary by value (descending) and then by
+        # key (ascending)
+        sorted_counts = sorted(
+            counts.items(), key=lambda item: (-item[1], item[0])
+        )
         for word, count in sorted_counts:
             if count > 0:
                 print(f"{word}: {count}")
-
