d2462b3af54c1ce5405571e9bab5ff3c9446268f
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-06-05 03:57:05 -0700

    Queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,34 +2,24 @@
 """
 Contains the number_of_subscribers function
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers
-    for a given subreddit.
-
-    :param subreddit: the name of the subreddit
-    :return: number of subscribers (int), or 0 if the subreddit is invalid
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        "User-Agent": "0x16-api_advanced:project: \
-                v1.0.0 (by /u/firdaus_cartoon_jr)"
-    }
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    """Returns the number of subscribers for a given subreddit"""
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
-
+    
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}
+    
     try:
-        data = response.json().get("data")
-        subscribers = data.get("subscribers")
-        return subscribers
-    except ValueError:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data.get("data", {}).get("subscribers", 0)
+        else:
+            return 0
+    except requests.RequestException:
         return 0
-
-
-if __name__ == "__main__":
-    print(number_of_subscribers(sys.argv[1]))
