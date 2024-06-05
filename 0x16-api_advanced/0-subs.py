1dbcf132ddd5efb4c4b3c68c3b299cec35e9723f
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-06-05 04:02:18 -0700

    Queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
     if subreddit is None or not isinstance(subreddit, str):
-        return 0
+        return 0, 'OK'
 
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': '0x16-api_advanced:project: \
