755d9d79fd4aef26ace59490bcdd370529e37277
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-06-06 14:46:45 -0700

    Queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,5 +12,7 @@ def number_of_subscribers(subreddit):
     if response.status_code == 200:
         data = response.json()
         return data["data"]["subscribers"]
+        print("OK")
     else:
         return 0
+        print("OK")
