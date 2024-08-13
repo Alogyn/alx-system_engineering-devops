031277857912442d91a9138642c13abcfe9a689b
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-08-13 07:34:42 -0700

    Query subscribers on a given Reddit subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,14 +8,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit"""
-    headers = {'User-Agent': 'reddit-subscriber-counter'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except Exception:
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
+
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
         return 0
