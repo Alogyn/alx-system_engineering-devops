75acc1136c2778330081f21c76ad169568c3eefa
Mohamed Derfoufi <138708102+Alogyn@users.noreply.github.com>
2024-08-13 14:34:19 +0100

    Query subscribers on a given Reddit subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""Function to query subscribers on a given Reddit subreddit"""
 import requests
 
 headers = {"User-Agent": "MyCustomUserAgent/1.0"}
@@ -8,10 +8,10 @@ headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data'].get('subscribers', 0)
-        return subscribers
+    request = requests.get(
+        url,
+        headers={"User-Agent": "Custom"},
+    if request.status_code == 200:
+        return request.json().get("data").get("subscribers")
     else:
         return 0
