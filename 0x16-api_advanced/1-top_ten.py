0c1df3e1d98b7772d51c6618e3b02826bc14ce18
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-08-13 08:02:41 -0700

    Contains the top_ten function

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,18 +12,29 @@ def top_ten(subreddit):
 
     :param subreddit: the name of the subreddit
     """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    # Set a custom User-Agent to avoid "Too Many Requests" errors
+    headers = {'User-Agent': 'reddit-top-ten'}
+
+    # Form the URL for the subreddit's hot posts with a limit of 10
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+
+    try:
+        # Make a GET request to the Reddit API
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        # Check if the response status code is 200 (OK)
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json()
+            posts = data['data']['children']
+
+            # Print the titles of the first 10 hot posts
+            for post in posts:
+                print(post['data']['title'])
+        else:
+            # If not 200, print None (either invalid subreddit or some other error)
+            print(None)
+    except Exception:
+        # In case of any request errors, print None
+        print(None)
+
