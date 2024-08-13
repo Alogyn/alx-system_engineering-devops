69796e5a1b7c991f5dba96432368c41437cb3fcc
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-08-13 07:46:23 -0700

    Contains the top_ten function

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -14,7 +14,7 @@ def top_ten(subreddit):
     """
     req = requests.get(
         "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
+        headers={"User-Agent": "Mozilla/5.0"},
         params={"limit": 10},
     )
 
