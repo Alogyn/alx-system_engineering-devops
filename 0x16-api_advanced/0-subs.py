c6c5e3b4db88da99905cdea4ddf385cae47f72d5
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-08-13 08:41:36 -0700

    Query subscribers on a given Reddit subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,14 +7,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'user-agent': 'MyRedditApp/0.0.1'}
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-    data = response.json().get('data')
-    if data is None:
-        return 0
-    num_subs = data.get('subscribers', 0)
-    return num_subs
