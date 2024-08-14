b678f40b45c2d73c4bdc7f24254084f9ddfb98b5
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-08-14 00:41:43 -0700

    Query subscribers on a given Reddit subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,7 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Alogyn)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
+    if response.status_code == 403:
+        return Ok
     results = response.json().get("data")
     return results.get("subscribers")
